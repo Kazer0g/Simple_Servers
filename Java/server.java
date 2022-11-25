5305ab37d785e323f5c914251c32fc836d900d70
Kazer0g <kazerog.workspace@gmail.com>
2022-11-25 22:04:12 +0300

    Java Commit

--- Java/server.java
+++ Java/server.java
@@ -1,33 +1,3 @@
-# Simple_Servers
-
-## Python
-[main.py](Python/main.py)
-```python
-import socket
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind (('127.0.0.1', 2000))
-
-server.listen(4)
-
-print ('Working...')
-
-client_socket, address = server.accept()
-
-data = client_socket.recv(1024).decode('utf-8')
-print (data)
-
-client_socket.send('Connected'.encode('utf-8'))
-```
-
-## C\#
-## Rust
-## Pascal
-## C
-## C++
-## Java
-[server.java](Java/server.java)
-```java
 import java.net.*;
 import java.io.*;
 
@@ -50,11 +20,3 @@ public class server{
         
     }
 }
-```
-## ASM
-## NodeJs
-## Ruby
-## Erlang
-## Go
-
-## Haskell
